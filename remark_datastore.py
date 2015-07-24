6d84ae6fb311486f22fd95ca6a7043c80bfe6673
Robert <gevent@CSSI2015-14.local>
2015-07-24 12:32:26 -0400

    Done

--- remark_datastore.py
+++ remark_datastore.py
@@ -2,6 +2,7 @@
 from datetime import datetime
 from google.appengine.api import memcache
 from google.appengine.ext import ndb
+import random
 
 
 _LAST_GET_KEY_PREFIX = 'lastget'
@@ -32,10 +33,11 @@ def ReadRemarks(user_id):
   # this user checked (Remark.timestamp >= start_time). Order them by when they
   # were posted. Return the results as a list of tuples with (user, text, color).
   remarks = []
+  color_list = ['red', 'blue', 'yellow', 'magenta', 'lime']
+  i = random.randint(0, len(color_list))
   query = Remark.query(Remark.timestamp >= start_time).order(Remark.timestamp)
   for remark in query.fetch():
-    # TODO(cssi-cam-2015) Randomize the color so that each remark is different.
-    remarks.append((remark.user, remark.text, 'black'))
+    remarks.append((remark.user, remark.text, color_list[i]))
 
   return remarks
 
